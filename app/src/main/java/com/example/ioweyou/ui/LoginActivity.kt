5469be40417c6fa1e57903b2dccc8bfcc416d35e
Neeraj <neeraj@beatroute.io>
2022-02-13 12:53:01 +0530

    login flow created for registered user

--- app/src/main/java/com/example/ioweyou/ui/LoginActivity.kt
+++ app/src/main/java/com/example/ioweyou/ui/LoginActivity.kt
@@ -1,20 +1,16 @@
 package com.example.ioweyou.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.text.TextUtils
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.ioweyou.R
 import com.example.ioweyou.base.BaseActivity
-import com.example.ioweyou.models.User
 import com.example.ioweyou.viewModel.MainViewModel
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import android.util.Patterns
+
 
 class LoginActivity : BaseActivity() {
     lateinit var viewModel: MainViewModel
@@ -27,31 +23,50 @@ class LoginActivity : BaseActivity() {
         ViewModelProvider.AndroidViewModelFactory.getInstance(application))
             .get(MainViewModel::class.java)
 
+        //insert user data manually in DB
 /*        viewModel.insertUser(User(1, "Neeraj", "neeraj@gmail.com", null, null, "123"))
         viewModel.insertUser(User(2, "Neeraj1", "neeraj@gmail.com", null, null, "123"))
         viewModel.insertUser(User(3, "Neeraj2", "neeraj@gmail.com", "male", 29, "123"))
         viewModel.insertUser(User(4, "Neeraj3", "neeraj@gmail.com", "male", 29, "123"))*/
 
-        viewModel.getUser("baburao@gmail.com").observe(this, Observer {
-            if (it != null)
-            showToast(it.userName)
-        })
-
     }
 
     fun login(view: android.view.View) {
+        //getting value from edit texts
+        val enteredValue = etUserName.text.toString().trim()
+        val password = etPass.text.toString().trim()
 
-        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-//        delay(4000)
-        showProgressDialogWithMessage("Loading...")
-        finish()
-        hideProgressDialog()
+        if (TextUtils.isEmpty(enteredValue)){
+            showToast(getString(R.string.enter_email))
+        } else if (!isValidEmail(enteredValue)) {
+            showToast(getString(R.string.invalid_input))
+        } else if (!TextUtils.isEmpty(password)){
+            viewModel.getUser(enteredValue).observe(this, Observer {
+                if (it != null && it.password.equals(password)) {
+                    showToast("Welcome ${it.userName}")
+                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                    finish()
+                } else {
+                    showToast(getString(R.string.invalid_credentials))
+                }
+            })
+        } else{
+            showToast(getString(R.string.enter_password))
+        }
 
-/*        MainScope().launch(Dispatchers.Main) {
-            delay(4000)
-            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-            delay(4000)
-            hideProgressDialog()
-        }*/
+    }
+
+    // the function which triggered when the LOGIN button is clicked
+    // which validates the email address entered by the user
+    fun isValidEmail(enteredValue: String): Boolean {
+
+        // extract the entered data from the EditText
+
+
+        // Android offers the inbuilt patterns which the entered
+        // data from the EditText field needs to be compared with
+        // In this case the the entered data needs to compared with
+        // the EMAIL_ADDRESS, which is implemented same below
+        return !enteredValue.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(enteredValue).matches()
     }
 }
