86ec25baaa164b8dca3d8082e98f401d4b952e62
neeraj <neeraj@beatroute.io>
2022-02-21 12:08:42 +0530

    unuser import removed

--- app/src/main/java/com/example/ioweyou/viewModel/ExpensesViewModel.kt
+++ app/src/main/java/com/example/ioweyou/viewModel/ExpensesViewModel.kt
@@ -1,20 +1,12 @@
 package com.example.ioweyou.viewModel
 
-import android.app.Application
 import androidx.lifecycle.*
-import com.example.ioweyou.database.IoweYouDatabase
 import com.example.ioweyou.models.Expenses
 import com.example.ioweyou.repository.ExpenseRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class ExpensesViewModel(private val expenseRepository: ExpenseRepository) : ViewModel() {
-/*    private var expenseRepository: ExpenseRepository
-
-    init {
-        val expensesDao = IoweYouDatabase.getDatabase(application).getExpenses()
-        expenseRepository = ExpenseRepository(expensesDao)
-    }*/
 
     fun getExpenseById(id: Int) = expenseRepository.getExpenseById(id)
 
